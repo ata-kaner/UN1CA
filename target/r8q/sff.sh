799d1f768468b7043a7d7238d72147790b10cc41
utkustnr <utkuustuner@proton.me>
2025-11-18 17:40:19 +0100

    r8q: patches: hfr: init
    Change-Id: I00f45980b2da9928a095ca020b8f2e4451dd790e

--- target/r8q/sff.sh
+++ target/r8q/sff.sh
@@ -20,5 +20,8 @@
 # Enable Video brightness feature
 SEC_FLOATING_FEATURE_COMMON_SUPPORT_HDR_EFFECT=true
 
+# Enable seamless refresh rate feature
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
+
 # Enable extra brightness feature
 SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
